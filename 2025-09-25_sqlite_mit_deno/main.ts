834d354ce5bb4620b956886c4048a0c24e5876f1
Nicolai Djambazov-Bichler <deine.email@example.com>
2025-10-02 06:32:51 -0700

    geygy

--- 2025-09-25_sqlite_mit_deno/main.ts
+++ 2025-09-25_sqlite_mit_deno/main.ts
@@ -1,5 +1,9 @@
 import { DatabaseSync } from "node:sqlite";
 
+import * as demo from "node:sqlite";
+
+demo.
+
 const db = new DatabaseSync("2ahwii.db");
 const stmt = db.prepare("SELECT * FROM students");
 const rows = stmt.all();
